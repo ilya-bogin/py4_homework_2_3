a14e193be553e356cb886fecb6644b1ce342de07
ilya-bogin <il.bogin@yandex.ru>
2017-06-01 00:47:14 +0300

    Improve output

--- get_top_words.py
+++ get_top_words.py
@@ -8,6 +8,10 @@ news_files  = [
   {'file' : './newsfr.json', 'encoding' : 'iso8859_5'},
   {'file' : './newsit.json', 'encoding' : 'CP1251'}]
 
+
+blacklisted_words = ['country', 'votpusk', '__cdata', 'kurort']
+
+
 def read_rss_items_from_file(file_name, encoding_type):
   with codecs.open(file_name, encoding=encoding_type) as data:
     rss = json.load(data)
@@ -21,10 +25,10 @@ def convert_rss_items_to_words(rss_items):
   return words
 
 
-def caculate_long_words_frequency(words):
+def caculate_long_words_frequency(words, blacklisted_words):
   frequency_map = {}
   for word in words:
-    if len(word) >= 6:
+    if len(word) >= 6 and word not in blacklisted_words:
       frequency_map[word] = frequency_map.get(word, 0) + 1
   return frequency_map
 
@@ -39,9 +43,11 @@ def main():
     print('Parse {}'.format(news_file['file']))
     rss_items = read_rss_items_from_file(news_file['file'], news_file['encoding'])
     all_words.extend(convert_rss_items_to_words(rss_items))
-  frequency_map = caculate_long_words_frequency(all_words)
+  frequency_map = caculate_long_words_frequency(all_words, blacklisted_words)
   top_ten_words = get_top_items_from_frequency_map(frequency_map, 10)
-  print(top_ten_words)
+  print('\nTop ten words by frequency:')
+  for word in top_ten_words:
+    print("{0}:{1}".format(word, frequency_map[word]))
 
 
 main()
