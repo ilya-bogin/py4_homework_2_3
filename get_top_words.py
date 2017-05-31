5603b97fe36a143bb9b985d7300a036ad714c0b5
ilya-bogin <il.bogin@yandex.ru>
2017-06-01 00:20:34 +0300

    Build frequency map

--- get_top_words.py
+++ get_top_words.py
@@ -1,5 +1,6 @@
 import codecs
 import json
+import re
 
 news_files  = [
   './newsfr.json', 
@@ -7,11 +8,34 @@ news_files  = [
   './newsfr.json',
   './newsfr.json']
 
-for news_file in news_files:
-  print('Parse {}'.format(news_file))
-  with codecs.open(news_file, encoding="iso8859_5") as data:
+def read_rss_items_from_file(file_name):
+  with codecs.open(file_name, encoding="iso8859_5") as data:
     rss = json.load(data)
-    rss_items = rss['rss']['channel']['item']
-    for rss_item in rss_items:
-      words = str(rss_item['description']).split()
-      print(words)
+    return rss['rss']['channel']['item']
+
+
+def convert_rss_items_to_words(rss_items):
+  words = []
+  for rss_item in rss_items:
+    words.extend(re.split('\W+', str(rss_item['description'])))
+  return words
+
+
+def caculate_long_words_frequency(words):
+  frequency_map = {}
+  for word in words:
+    if len(word) >= 6:
+      frequency_map[word] = frequency_map.get(word, 0) + 1
+  return frequency_map
+
+
+def main():
+  for news_file in news_files:
+    print('Parse {}'.format(news_file))
+    rss_items = read_rss_items_from_file(news_file)
+    words = convert_rss_items_to_words(rss_items)
+    frequency_map = caculate_long_words_frequency(words)
+    print(frequency_map)
+
+
+main()
